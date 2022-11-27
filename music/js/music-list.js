6f7e11c60c3c9fdc6e260fd7bc597995c19092ed
Desiad <103303799+Desiad@users.noreply.github.com>
2022-11-27 11:20:10 +0600

    Add files via upload

---
+++ music/js/music-list.js
@@ -0,0 +1,94 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Tere Sang Yaara",
+    artist: "Rustom 2016",
+    img: "cover",
+    src: "music-1"
+  },
+  {
+    name: "Dil Ko Karaar Aaya",
+    artist: "Neha Kakkar",
+    img: "cover",
+    src: "music-2"
+  },
+  {
+    name: "Tip Tip",
+    artist: "Sooryavanshi",
+    img: "cover",
+    src: "music-3"
+  },
+  {
+    name: "Meri Zindagi Hai Tu",
+    artist: "Satyameva Jayate 2",
+    img: "cover",
+    src: "music-4"
+  },
+  {
+    name: "Raatan Lambiyan",
+    artist: "Sershah",
+    img: "cover",
+    src: "music-5"
+  },
+    {
+    name: "Tumko Barish Pasand Hai",
+    artist: "Neha Kakkar",
+    img: "cover",
+    src: "music-6"
+  },
+    {
+    name: "Tomar_Jonno",
+    artist: "Arnob",
+    img: "cover",
+    src: "music-7"
+  },
+    {
+    name: "Shunil_Boruna",
+    artist: "Mahadi",
+    img: "cover",
+    src: "music-8"
+  },
+    {
+    name: "Gale Lag Ja",
+    artist: "De Dana Dan",
+    img: "cover",
+    src: "music-9"
+  },
+      {
+    name: "Keno Pichu Dako Amare",
+    artist: "Arijit Singh",
+    img: "cover",
+    src: "music-10"
+  },
+    {
+    name: "Chogada",
+    artist: "Loveratri",
+    img: "cover",
+    src: "music-11"
+  },
+        {
+    name: "Ei_Bhalo_Ei_Kharap",
+    artist: "Arijit Singh",
+    img: "cover",
+    src: "music-12"
+  },
+
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
